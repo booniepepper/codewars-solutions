1a423e8cb0fbcac94738f6e51dc333f057b0a731
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-01 01:22:17 -0700

    fill in vb solutions

---
+++ vb/multiply/solution.vb
@@ -0,0 +1,8 @@
+' https://www.codewars.com/kata/50654ddff44f800200000004
+
+Public Module Example
+  Public Function Multiply(ByVal a As Integer, ByVal b As Integer) As Integer
+    Return a * b
+  End Function
+End Module
+
---
+++ vb/sum-of-odd-numbers/solution.vb
@@ -0,0 +1,8 @@
+' https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+
+Public Module Kata
+    Public Function RowSumOddNumbers(ByVal n As Integer) As Integer
+        Return n * n * n
+    End Function
+End Module
+
